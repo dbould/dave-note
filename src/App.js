19802418d4811b99e600a4855c13efdd9bd195b3
dave <dave@bould.net>
2017-05-31 10:42:32 +0100

    Added default values from first result from api lookup of all notes json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import Notebook from './Notebook.js';
-import NoteTitle from './NoteTitle.js';
-import Editor from './Editor.js';
 
 class App extends Component {
   render() {
     return (
-      <div id="main-container">
+      <div>
         <Notebook />
-        <NoteTitle />
-        <Editor />
       </div>
     );
   }
--- src/Editor.js
+++ src/Editor.js
@@ -5,9 +5,9 @@ import '../styles/editor.css';
 class Editor extends Component {
   constructor(props) {
       super(props);
-      this.state = {noteText: null};
-
+      this.state = {noteText: ''};
       this.handleChange = this.handleChange.bind(this);
+      this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
   }
 
   handleChange(event) {
@@ -15,11 +15,19 @@ class Editor extends Component {
     this.setState({noteText: noteText});
   }
 
+  componentWillReceiveProps(props) {
+    if (props.noteText !== this.state.noteText) {
+      this.setState({noteText: props.noteText});
+      this.forceUpdate();
+    }
+  }
+
   render() {
     return (
       <div>
         <div className={"editor"}>
-          <textarea onChange={this.handleChange}>
+          <textarea onChange={this.handleChange} value={this.state.noteText}>
+
           </textarea>
         </div>
         <Preview noteText={this.state.noteText} />
